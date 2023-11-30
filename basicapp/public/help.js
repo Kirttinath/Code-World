87e31936e42722ecabaca22a4ec28daf6ec41ba8
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-30 20:36:10 +0530

    Guidance for React app Installation

---
+++ basicapp/public/help.js
@@ -0,0 +1,25 @@
+/*Next.js is a full-stack React framework. It’s versatile and lets you create React apps of any size—from a mostly static blog to a complex dynamic application. To create a new Next.js project, run in your terminal:
+
+Create a new Project npx :  create-next-app@latest
+
+                         :  npx create-react-app projectname
+then go to the file location means project nname folder i.e : cd projectname
+the start the app by command : npm start
+
+
+ ** Grab react and react-dom from npm, set up your custom build process with a bundler like Vite or Parcel, and add other tools as you need them for routing, static generation or server-side rendering, and more.
+
+ For Vite :
+       Vite (French word for "quick", pronounced /vit/, like "veet") is a build tool that aims to provide a faster and leaner development experience for modern web projects. It consists of two major parts:
+
+A dev server that provides rich feature enhancements over native ES modules, for example extremely fast Hot Module Replacement (HMR).
+
+A build command that bundles your code with Rollup, pre-configured to output highly optimized static assets for production.
+
+command : npm create vite@latest
+
+In vite we have install npm for run the app
+So we go to that folder i.e cd projectname
+command : npm install
+then we can run the app by command : npm run dev
+*/
