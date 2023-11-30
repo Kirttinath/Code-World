0f343d4727c9c52d4cdf31a0f14f307179c4295a
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-11-30 20:49:34 +0530

    importing modules and files

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,10 +1,8 @@
 
-function App() {
-  return (
-    <>
-    <b>Hello world! Guys</b>
-    </>
-  );
-}
+import { useState } from 'react'
+import './App.css'
+import Search from './components/search/search'
+import CurrentWeather from './components/current-weather/current-weather'
+import Forecast from './components/forecast/forecast'
+import { WEATHER_API_URL, WEATHER_API_KEY } from './api'
 
-export default App;
