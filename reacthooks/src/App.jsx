e582ec68f3c89217895b786f13f9f7bf8e618e8e
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-01 16:37:07 +0530

    create a new function and set the new buttons of like, comment and shares

--- reacthooks/src/App.jsx
+++ reacthooks/src/App.jsx
@@ -2,7 +2,7 @@ import react,{ useState } from 'react'
 
 
 
-function App() {
+/* function App() {
   const [count, setCount] = useState(0);
   const [theme, setTheme]  = useState("Blue");
 
@@ -23,6 +23,23 @@ function increase()
     <button onClick={increase}>+</button>
     </>
   )
+} */
+
+function App1()
+{
+
+  const [curr,newcurr]= useState("show");
+
+  return(
+    <>
+    <div>
+    <button onClick={() => newcurr("Likes")}>Like</button>
+    <button onClick={() => newcurr("Comments")}>Comment</button>
+    <button onClick={() => newcurr("Shares")}>Share</button>
+    </div>
+    <h1>{curr}</h1>
+    </>
+  )
 }
 
-export default App
+export default App1
