0b78f2a196c855095f469867ea557b39fc6f7805
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-01 16:12:06 +0530

    some points about useState and useEffect hooks

---
+++ reacthooks/src/help.jsx
@@ -0,0 +1,18 @@
+/*
+useState and useEffect
+
+What is useState :
+___________________
+
+- It's a hook that allows you to add state variables in functional components.
+ The React useState Hook allows us to track state in a function component.
+State generally refers to data or properties that need to be tracking in an application.
+
+What is useEffect :
+__________________
+
+ `useEffect` is a React Hook that allows you to perform side effects in your components. Some examples of side effects are: fetching data, directly updating the DOM, and timers. 
+
+The `useEffect` hook accepts two arguments: a **function** and an **optional dependency array** ¹². The function is executed after every render of the component. 
+The dependency array is used to specify the variables that the function depends on. If any of the variables in the dependency array change, the function is re-executed ¹².
+ */
