6e83588f8f690413fdabfd41c5542479d89627f5
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-02 14:37:04 +0530

    How to put a component in same file without importing it

--- reacthooks/src/main.jsx
+++ reacthooks/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-//import User from './User.jsx'
 import App from './App.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <User/>
+    <User/>
+    <User/>
   </React.StrictMode>
 )
+
+function User() {
+  return (
+    <>
+    <h1>Hello world</h1>
+    </>
+  )
+}
