d67c27a17c94753a4bd58044997e0af5eb0868fb
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-05 18:58:24 +0530

    styling in items component

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -7,7 +7,7 @@ const Item = () => {
             <img src={props.image} alt="" />
             <p>{props.name}</p>
             <div className="item-prices">
-                <div className="item-proice-new">
+                <div className="item-price-new">
                     {props.new_price}
                 </div>
                 <div className="item-price-old">
