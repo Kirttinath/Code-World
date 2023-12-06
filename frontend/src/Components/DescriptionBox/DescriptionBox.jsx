0482c3774b4086efc0101fb39b938c15d078b6a3
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-06 23:20:18 +0530

    description jsx and css files are added to reviews and description

--- frontend/src/Components/DescriptionBox/DescriptionBox.jsx
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -6,11 +6,10 @@ const DescriptionBox = () => {
     <div className='descriptionbox'>
       <div className="descriptionbox-navigator">
         <div className="descriptionbox-nav-box">Description</div>
-        <div className="description-nav-box fade">Revies (145)</div>
+        <div className="descriptionbox-nav-box fade">Reviews (145)</div>
       </div>
       <div className="descriptionbox-description">
         <p>An Ecommerce website is an online pltform that facilitates the buying and selling of products or services over the internet.It serves as a virtual marketplace where buisnesses and individuals can showcase their products, interact with customers, and conduct transactions without the need for a physical presence E-commerce websitehave gained immense popularity due to their convinience, accessibility, and the golden reach they offer.</p>
-        <p></p>
       </div>
     </div>
   )
