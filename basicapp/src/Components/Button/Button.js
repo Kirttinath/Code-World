6877bf4d4ef5653f719e4bf1a05bb11d52700d3d
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-13 23:51:37 +0530

    Changed the import path for the Button component and deleted the Button component file.

---
+++ basicapp/src/Components/Button/Button.js
@@ -0,0 +1,3 @@
+import { Button } from "./Buttonstyles"
+
+export default Button
--- basicapp/src/Components/Button/Buttonstyles.js
+++ basicapp/src/Components/Button/Buttonstyles.js
@@ -1,7 +1,7 @@
 
 import styled from "styled-components";
 
-const Button = styled.button`
+export const Button = styled.button`
   border: 2px solid #4caf50;
   background-color: #4caf50;
   padding: 15px 32px;
@@ -12,4 +12,3 @@ const Button = styled.button`
   cursor: pointer;
   transition: 0.5s all ease-out;
   `
-export default Button
