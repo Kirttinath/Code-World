2866471d7713c015aa9d0f0e1698be62700af1dc
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-13 23:46:50 +0530

    Replace the HTML button element with a styled component.

--- basicapp/src/App.js
+++ basicapp/src/App.js
@@ -1,23 +1,12 @@
-
+import Button from "./Components/Button";
 import "./App.css";
-import styled from "styled-components";
 
-const Button = styled.button`
-  border: 2px solid #4caf50;
-  background-color: #4caf50;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  cursor: pointer;
-  transition: 0.5s all ease-out;
-  `
+
+
 
 function App() {
   return (
     <div className='App'>
-      {/* <button>Button</button> */}
       <Button>Styled Component</Button>
     </div>
   )
---
+++ basicapp/src/Components/Button.js
@@ -0,0 +1,15 @@
+
+import styled from "styled-components";
+
+const Button = styled.button`
+  border: 2px solid #4caf50;
+  background-color: #4caf50;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+  transition: 0.5s all ease-out;
+  `
+export default Button
