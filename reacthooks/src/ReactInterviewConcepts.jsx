241c82dfacddccf1a2092247e8d506da6f63bf6d
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-09 21:10:56 +0530

    Topics Added

---
+++ reacthooks/src/ReactInterviewConcepts.jsx
@@ -0,0 +1,104 @@
+/*
+ReactJs Interview Topics with Concepts :✅📌
+_____________________________________________
+
+
+1.📌Hooks :
+-----------
+📍useState
+📍useEffect
+📍useContext
+📍useReducer
+📍useMemo
+📍useCallback
+📍useRef
+
+2.📌Higher Order Components :
+-------------------------------
+📍What ?
+📍When ?
+📍Why ?
+📍How ?
+
+3.📌Life Cycle methods of Components :
+------------------------------------------
+📍CallComponents
+📍3phases : Mounting
+             Updating
+             Unmounting
+
+4.📌State Management(All about Data) :
+---------------------------------------
+📍State / Props
+📍Props Drilling
+📍Context / Context API
+
+5.📌Redux (with React-redux library) / Zustand :
+-----------------------------------------------------
+📍How Redux works
+📍Why ?
+📍When ?
+📍Redux Toolkit(RTK)
+
+
+6.📌Custom Hooks : (Clean, Maintainable, Readable, Reusable)
+---------------------------------------------------------------
+📍When to use it ?
+📍Code (how to use it ?)
+📍Examples
+
+7.📌Lazy Loading :
+----------------------
+📍Code spliting
+📍Chunking
+📍Suspense
+
+8.📌Virtual DOM :
+-------------------
+📍Reconciliation Alg
+📍React Fibre
+📍Diffing Alogorithm works
+📍How Render works
+
+9.📌SSR vs CSR :
+------------------
+📍Server Side Rendering
+📍Client Side Rendering 
+📍Difference
+📍SEO and performance
+
+10.📌Routing (RBAC) :
+------------------------
+📍Role Base Access Control
+📍react-router
+📍Protected Routes
+📍Dynamic Router
+
+11.📌Testing (Unit Testing):
+----------------
+📍React Testing Library
+
+
+12.📌Async Task :
+-------------------
+📍API calls
+📍Events
+📍Promises
+
+
+13.📌Reusability / Modularity / Testability :
+---------------------------------------------
+📍Coding Practices
+📍Components Patterns
+
+
+
+
+10.🚀Performance Optimization Techniques:
+
+
+
+
+
+
+*/
